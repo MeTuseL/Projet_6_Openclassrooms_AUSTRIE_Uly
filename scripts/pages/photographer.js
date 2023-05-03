97018246e650b9fe9d7cade18c534af75d95323a
Uly AUSTRIE <austrie-uly@hotmail.fr>
2023-05-03 04:34:35 -0400

    fix: media video

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -61,33 +61,57 @@ fetch('data/photographers.json')
             //events media video
             const iconVideo = document.querySelectorAll(".icon-play-media");
             const video = document.querySelectorAll(".video-media");
-            
-            iconVideo.forEach((icon,index) => icon.addEventListener("click", () => {
+
+            iconVideo.forEach((icon, index) => icon.addEventListener("click", () => {
                 video[index].play().then(() => {
                     icon.style.display = "none";
                     video[index].setAttribute("controls", "");
                     video[index].style.filter = "blur(0)";
                 });
             }));
-           
-            video.forEach((vid,index) => vid.addEventListener("ended", () => {
+
+            video.forEach((vid, index) => vid.addEventListener("pause", () => {
+
+                //The video returns to its display by default, 
+                // after 5 seconds it is finished (without the user wanting to read it again)
+                    if (vid.currentTime == vid.duration
+                        && vid.ended) {
+                        setTimeout(() => {
+                            breakme: if (vid.currentTime !== vid.duration
+                                && !vid.ended) {
+
+                                break breakme;
+                            }
+                            else {
+                                if(document.fullscreenElement){
+                                    document.exitFullscreen();
+                                }
+                                iconVideo[index].style.display = "block";
+                                vid.removeAttribute("controls");
+                                vid.style.filter = "blur(2px)";
+                            }
+                        }, 5000);
+                    }
+                    //The video returns to its display by default, 
+                    // after 15 seconds it is paused (without the user wanting to read it again)
+                    if(vid.paused){
+                            setTimeout(() => {
+                                breakme: if (!vid.paused) {
+    
+                                    break breakme;
+                                }
+                                else {
+                                    if(document.fullscreenElement){
+                                        document.exitFullscreen();
+                                    }
+                                    iconVideo[index].style.display = "block";
+                                    vid.removeAttribute("controls");
+                                    vid.style.filter = "blur(2px)";
+                                }
+                        },15000);
+                    }
                 
-                if (document.fullscreenElement) {
-                    setTimeout(() => {
-                        document.exitFullscreen();
-                        iconVideo[index].style.display = "block";
-                        vid.removeAttribute("controls");
-                        vid.style.filter = "blur(2px)";
-                    },5000);
-                    
-                }
-                else{
-                    setTimeout(() => {
-                    iconVideo[index].style.display = "block";
-                    vid.removeAttribute("controls");
-                    vid.style.filter = "blur(2px)";
-                },2000); 
-            }                  
+
             }));
 
         }
